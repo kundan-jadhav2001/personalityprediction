5a41fccd7c4affc70192b698469f8f558b14afaf
Kundan <kundanjadhav2001@gmail.com>
2023-09-03 22:20:37 +0530

    test form

--- coders/views.py
+++ coders/views.py
@@ -8,4 +8,16 @@ def test(request):
     return render(request, 'test.html')
 
 def result(request):
+    if request.method == "POST":
+        name = request.POST['name']
+        age = request.POST['age']
+        gender = request.POST['gender']
+
+        openness = request.POST['openness']
+        neuroticism = request.POST['neuroticism']
+        conscientiousness = request.POST['conscientiousness']
+        agreeableness = request.POST['agreeableness']
+        extraversion = request.POST['extraversion']
+
+        
     return render(request, 'result.html')
